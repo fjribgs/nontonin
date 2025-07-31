d09052a28f0d6d1991ac2deb2faeab75cb212739
fjribgs <fajribagaskara@upi.edu>
2025-07-31 17:37:37 +0700

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import '../index.css'
+
+function Hero() {
+  return (
+    <div id="hero" className='bg-[var(--primary5)] h-100'>
+      <div id="hero-left"></div>
+      <div id="hero-right"></div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import '../index.css'
+
+function Navbar() {
+  return (
+    <nav className="bg-[var(--primary5)] text-[var(--primary1)] flex 
+    place-content-between h-18 w-full p-9 sm:px-26 items-center">
+      <div id="navbar-left" className="font-bold text-2xl">fjribgs.</div>
+      <div id="navbar-center" className="hidden sm:block justify-center">
+        <ul className="flex gap-9">
+          <li className='cursor-pointer duration-200 ease-in hover:font-bold'><a href="#">Home</a></li>
+          <li className='cursor-pointer duration-200 ease-in hover:font-bold'><a href="#">Review</a></li>
+          <li className='cursor-pointer duration-200 ease-in hover:font-bold'><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
