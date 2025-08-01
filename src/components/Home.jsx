0c31af27189deadcce574119c340a7a85c4753a7
fjribgs <fajribagaskara@upi.edu>
2025-08-01 15:39:17 +0700

    adjust settings

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,30 +20,37 @@ function Hero() {
     place-content-between pl-9 sm:pl-25 sm:pr-25'>
       <div id="hero-left" className='z-10'>
         <img src={laios} alt="laios" className='size-48 rounded-3xl mb-7 block sm:hidden'/>
+        
         <div id="title" className='font-bold text-6xl sm:text-[120px]
         text-[var(--primary1)] '>Hello!</div>
+        
         <div id="subtitle" className='font-bold md:pl-2 text-[21px] 
         sm:text-[45px] text-[var(--primary1)] pl-1 -mt-1.5 sm:-mt-3 
         '><span className='font-medium'>I'm a</span><span 
           className='transition-all duration-3000 ease-in'> {roles[index]}</span>
         </div>
+        
         <div id="description" className='mt-4 sm:mt-8 pr-10 pl-1 
         text-[var(--primary1)] max-w-80 sm:pl-2 sm:max-w-160 
         sm:text-[20px]'>I'm <span className='font-semibold'>Fajri Bagaskara 
         Budi</span>, I build responsive, user-friendly websites with clean 
         code and thoughtful design.</div>
+
         <div id="button" className='flex'>
           <a id="my-resume" className='border-2 py-3 px-5 mt-5 ml-2 rounded-3xl 
           bg-[var(--primary3)] border-[var(--primary3)] text-[var(--primary1)]
           hover:bg-[var(--primary5)] duration-150 ease-in'>My Resume</a>
         </div>
       </div>
+
       <div id="hero-right" className="size-130 hidden sm:hidden ml-10 items-center
       z-10">
         <img src={laios} alt="laios" className='rounded-3xl mb-7 
         hidden sm:block'/>
       </div>
-      <img src={laiosbg} alt="laiosbg" className='absolute ml-30 h-254'/>
+
+      <img src={laiosbg} alt="laiosbg" className='absolute ml-30 h-254 hidden
+      sm:block'/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import '../index.css'
 import githubwhite from '../assets/github-white.svg'
 import linkedinwhite from '../assets/linkedin-white.svg'
+import laios from '../assets/laios.jpeg'
 
 function Navbar() {
   return (
     <nav className="bg-[var(--primary5)] text-[var(--primary1)] flex 
-    place-content-between h-18 w-full p-9 sm:px-26 items-center fixed">
+    place-content-between h-18 w-full p-9 sm:px-26 items-center fixed
+    z-50">
       <div id="navbar-left" className="font-bold text-2xl">fjribgs.</div>
       <div id="navbar-center" className="hidden sm:block justify-center">
         <ul className="flex gap-9">
@@ -14,11 +16,13 @@ function Navbar() {
           <li className='cursor-pointer duration-200 ease-in hover:font-bold'><a href="#">Contact</a></li>
         </ul>
       </div>
-      <div id="navbar-right" className='flex gap-2'>
-        <a href="#" className=''><img src={githubwhite} alt="" 
+      <div id="navbar-right" className='flex gap-2 items-center'>
+        <a href="https://github.com/fjribgs" className=''><img src={githubwhite} alt="" 
         className='size-6 lg:size-8'/></a>
-        <a href="#" className=''><img src={linkedinwhite} alt="" 
+        <a href="https://linkedin.com/in/fajribagas" className=''><img src={linkedinwhite} alt="" 
         className='size-6 lg:size-8'/></a>
+        <div id="img" className='mt-7 ml-3'><img src={laios} alt="laios" className='rounded-4xl mb-7 
+        hidden sm:block size-10'/></div>
       </div>
     </nav>
   )
